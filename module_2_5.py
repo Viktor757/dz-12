311af1eee002dd4d78df3bd88b6e7341c0cd5468
Viktor757 <vitek-594@yandex.ru>
2024-09-07 17:25:50 +0500

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,30 @@
+def get_matrix(n,m,Value):
+    matrix = []
+    for i in range(n):
+        w = []
+        matrix.append(w)
+        for j in range(m):
+            w.append(Value)
+    return matrix
+
+
+result1 = get_matrix(1,2,10)
+result2 = get_matrix(4,6,8)
+result3 = get_matrix(7,6,4)
+print(result1)
+print(result2)
+print(result3)
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
